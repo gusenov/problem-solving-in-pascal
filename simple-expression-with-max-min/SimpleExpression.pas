723f4b63bba2c269a1c4c3c95f5c91a9a091b1a8
Аббас Гусенов <gusenov@live.ru>
2018-02-06 18:47:42 +0600

    Разветвляющееся вычисление выражения Z = max(A, min(B, C, D)).

---
+++ simple-expression-with-max-min/SimpleExpression.pas
@@ -0,0 +1,52 @@
+﻿program SimpleExpression;
+var
+  A, B, C, D, Z: Integer;
+begin
+  write('A = ');
+  readln(A);
+  
+  write('B = ');
+  readln(B);
+  
+  write('C = ');
+  readln(C);
+  
+  write('D = ');
+  readln(D);
+  
+  if B < C then
+    if B < D then
+    
+      // B минимальный из B, C, D; т.к. B < C и B < D
+      if A > B then
+        Z := A
+      else
+        Z := B
+        
+    else
+    
+      // D минимальный из B, C, D; т.к. B < C, а D <= B
+      if A > D then
+        Z := A
+      else
+        Z := D
+        
+  else
+    if C < D then
+    
+      // C минимальный из B, C, D; т.к. C <= B и C < D
+      if A > C then
+        Z := A
+      else
+        Z := C
+        
+    else
+    
+      // D минимальный из B, C, D; т.к. C <= B и D <= C
+      if A > D then
+        Z := A
+      else
+        Z := D;
+        
+  writeln('Z = max(', A, ', min(', B, ', ', C, ', ', D, ')) = ', Z);
+end.
