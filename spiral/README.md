0e48dda147a672d12d4394ca88dda145aa4ae798
Аббас Гусенов <gusenov@live.ru>
2018-03-07 15:32:40 +0600

    Построение спирали вокруг начала координат.

---
+++ spiral/README.md
@@ -0,0 +1,13 @@
+# Задача
+
+<kbd>![Задача](problem-statement.jpg)</kbd>
+
+# Скриншот
+
+<kbd>![Спираль](screenshot.png)</kbd>
+
+# Ссылки
+
+- [Графика, графика, графика! Часть 1. Введение.](http://pascalabc.net/stati-po-pascalabc-net/obuchenie-programmirovaniyu/16-grafika-grafika-grafika-chast-1-vvedenie)
+- [Pascal спираль - Помощь студентам  - Форум программистов](http://www.programmersforum.ru/showpost.php?s=9e5526395b84fa9ca7f370a9568277cb&p=934419&postcount=6)
+- [Нарисовать спираль](https://vk.com/page-37477344_44493176)
---
+++ spiral/Spiral.pas
@@ -0,0 +1,50 @@
+﻿program Spiral;
+
+uses GraphABC;
+
+var
+  n: Integer;  // количество витков спирали.
+  
+  r: Integer;  // начальный радиус спирали.
+  rstep: Integer;  // шаг увеличения радиуса в пикселах.
+  
+  t: Integer;  // начальный угол поворота спирали.
+  tstep: Integer;  // шаг увеличения угла в градусах.
+  
+  x, y: Integer;
+  xold, yold: Integer;
+  xc, yc: Integer;  // координаты центра окна.
+
+begin
+
+  WriteLn('Введите количество витков спирали...');
+  ReadLn(n);
+
+  WriteLn('Введите начальный радиус спирали и шаг увеличения (в пикселах)...');
+  ReadLn(r, rstep);
+  
+  WriteLn('Введите начальный угол поворота спирали и шаг увеличения (в градусах)...');
+  ReadLn(t, tstep);
+
+  // Начало координат.
+  xc := Window.Width div 2;
+  yc := Window.Height div 2;
+
+  xold := xc;
+  yold := yc;
+
+  repeat
+    x := xc + round(r * cos(t * pi / 180));
+    y := yc - round(r * sin(t * pi / 180));
+    
+    MoveTo(xold, yold);
+    LineTo(x, y);
+    
+    xold := x;
+    yold := y;
+    
+    t := t + tstep;  // увеличиваем угол на tstep градусов.
+    r := r + rstep;  // увеличиваем радиус на rstep пикселов.
+  until t > n * 360;  // делаем n оборотов.
+
+end.
