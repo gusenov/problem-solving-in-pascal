d3426508ca22297827c0470ffa9eec927a34182f
Аббас Гусенов <gusenov@live.ru>
2018-05-17 14:28:06 +0600

    Определение соответствующего году номера столетия.

---
+++ century-number/CenturyNumber.pas
@@ -0,0 +1,28 @@
+﻿program CenturyNumber;
+
+function Input(): Integer;
+begin
+  result := 0;
+  repeat
+    Write('Введите номер года -> ');
+    ReadLn(result);
+    if result <= 0 then
+      WriteLn('Ошибка! Введите целое положительное число.');
+  until result > 0;
+end;
+
+function GetCentury(year: Integer): Integer;
+begin
+  result := year div 100;
+  if year mod 100 <> 0 then
+    Inc(result);
+end;
+
+var
+  year: Integer;  // номер года.
+  сentury: Integer;  // номер столетия.
+begin
+  year := Input();
+  сentury := GetCentury(year);
+  WriteLn('Соответствующий ' + year + ' году номер столетия: ' + сentury);
+end.
---
+++ century-number/README.md
@@ -0,0 +1,3 @@
+# Problem Statement
+
+![Определить соответствующий году номер столетия](problem-statement.png)
