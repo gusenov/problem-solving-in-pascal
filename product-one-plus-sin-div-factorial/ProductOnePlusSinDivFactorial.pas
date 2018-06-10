c9cc4185e28a47d2f043ad17ec14b24b0a61c420
Аббас Гусенов <gusenov@live.ru>
2018-06-10 11:48:58 +0600

    Программа product-one-plus-sin-div-factorial/ через процедуру и функцию.

--- product-one-plus-sin-div-factorial/ProductOnePlusSinDivFactorial.pas
+++ product-one-plus-sin-div-factorial/ProductOnePlusSinDivFactorial.pas
@@ -1,23 +1,31 @@
 ﻿program ProductOnePlusSinDivFactorial;
 
-var
-  n, k, f: Integer;
-  x, p: Real;
-
+procedure Input(var n: Integer; var x: Real);
 begin
   Write('n -> ');
   ReadLn(n);
   
   Write('x -> ');
   ReadLn(x);
-  
-  p := 1;
+end;
+
+function Product(n: Integer; x: Real): Real;
+var
+  k, f: Integer;
+begin
+  result := 1;
   f := 1;
   for k := 1 to n do begin
     f := f * k;
-    p := p * (1 + sin(k * x) / f);
+    result := result * (1 + sin(k * x) / f);
   end;
-  
-  WriteLn('P = ', p);
+end;
 
+var
+  n: Integer;
+  x: Real;
+
+begin
+  Input(n, x);  
+  WriteLn('P = ', Product(n, x));
 end.
